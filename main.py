bd1db93128bc298fe7abaa2a17a741e14d8a05ae
mh326 <m.hiroki326@gmail.com>
2024-03-17 00:08:22 +0900

    implement display, edit, delete todo

--- main.py
+++ main.py
@@ -1,21 +1,96 @@
-import flet as ft
+import flet
+from flet import (
+    Checkbox,
+    Column,
+    FloatingActionButton,
+    IconButton,
+    Page,
+    Row,
+    TextField,
+    UserControl,
+    colors,
+    icons,
+)
 
 
-class TodoApp(ft.UserControl):
+class Task(UserControl):
+    def __init__(self, task_name, task_delete):
+        super().__init__()
+        self.task_name = task_name
+        self.task_delete = task_delete
+
     def build(self):
-        self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
-        self.tasks = ft.Column()
+        self.display_task = Checkbox(value=False, label=self.task_name)
+        self.edit_name = TextField(expand=1)
+
+        self.display_view = Row(
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                self.display_task,
+                Row(
+                    spacing=0,
+                    controls=[
+                        IconButton(
+                            icon=icons.CREATE_OUTLINED,
+                            tooltip="Edit To-Do",
+                            on_click=self.edit_clicked,
+                        ),
+                        IconButton(
+                            icons.DELETE_OUTLINE,
+                            tooltip="Delete To-Do",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = Row(
+            visible=False,
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                self.edit_name,
+                IconButton(
+                    icon=icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        return Column(controls=[self.display_view, self.edit_view])
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def delete_clicked(self, e):
+        self.task_delete(self)
+
+
+class TodoApp(UserControl):
+    def build(self):
+        self.new_task = TextField(hint_text="Whats needs to be done?", expand=True)
+        self.tasks = Column()
 
         # application's root control (i.e. "view") containing all other controls
-        return ft.Column(
+        return Column(
             width=600,
             controls=[
-                ft.Row(
+                Row(
                     controls=[
                         self.new_task,
-                        ft.FloatingActionButton(
-                            icon=ft.icons.ADD, on_click=self.add_clicked
-                        ),
+                        FloatingActionButton(icon=icons.ADD, on_click=self.add_clicked),
                     ],
                 ),
                 self.tasks,
@@ -23,21 +98,26 @@ class TodoApp(ft.UserControl):
         )
 
     def add_clicked(self, e):
-        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        task = Task(self.new_task.value, self.task_delete)
+        self.tasks.controls.append(task)
         self.new_task.value = ""
         self.update()
 
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
 
-def main(page: ft.Page):
+
+def main(page: Page):
     page.title = "ToDo App"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.horizontal_alignment = "center"
     page.update()
 
     # create application instance
-    todo = TodoApp()
+    app = TodoApp()
 
     # add application's root control to the page
-    page.add(todo)
+    page.add(app)
 
 
-ft.app(target=main)
+flet.app(target=main)
